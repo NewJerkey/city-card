471a1ff3f5c4e415bab0d714a51ad29f7cae9fc0
NewJerkey <bryschneider1@gmail.com>
2023-02-16 13:20:44 -0500

    adding stuffs

--- src/city-card.js
+++ src/city-card.js
@@ -1,88 +1,158 @@
 import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+//const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class CityCard extends LitElement {
   static properties = {
-    header: { type: String },
+    title: { type: String },
+    cardCount: { type: Int16Array },
+    image: { type: String }
   }
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
+    body {
+      background: grey;
+    }
+    .card {
+      width: 300px;
       text-align: center;
-      background-color: var(--city-card-background-color);
+      border: 3px solid yellow;
+      margin-top: 40px;
+      margin-bottom: 40px;
+      margin-right: 40px;
+      margin-left: 80px;
+      padding-bottom: 16px;
+      padding-right: 8px;
+      padding-left: 8px;
+      box-shadow: 3px 3px;
+      border-radius: 25px;
+      background: blue;
     }
 
-    main {
-      flex-grow: 1;
+    .paragraph {
+      font-size: 18px;
+      color: white;
+      margin-top: 24px;
+      margin-bottom: 24px;
+      margin-right: 24px;
+      margin-left: 24px;
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
+    .header {
+      font: 38px Papyrus;
+      color: white;
+      margin-bottom: 16px;
     }
 
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
+    .buttons button {
+      border-radius: 25px;
+    }
+
+    .details {
+      border-radius: 25px;
+      color: yellow;
+      background-color: #4caf50;
+    }
+
+    button:hover,
+    button:focus {
+      background-color: yellow;
+    }
+
+    .image {
+      border: 3px solid yellow;
+      max-width: 250px;
+    }
+
+    @media (max-width: 800px) and (min-width: 500px) {
+      .button {
+        display: none;
       }
-      to {
-        transform: rotate(360deg);
+    }
+
+    @media (max-width: 500px) {
+      * {
+        transform: scale(0.8);
       }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.cardCount = 1;
+    this.title = 'Bryce Schneider';
+    this.image =
+      'https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png';
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/CityCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <div class="buttons">
+        <button class="copy" @click=${this.copy}>Duplicate</button>
+        <button class="changeBackground" @click=${this.changeBG}>
+          Change Background
+        </button>
+        <button class="changeTitle" @click=${this.changeTitle}>
+          Change Title
+        </button>
+        <button id="delete" @click=${this.delete}>Delete Last Instance</button>
+      </div>
+      <div class="card">
+        <h2 class="header">${this.title}</h2>
+        <img class="image" alt="Handsome Bryce" src=${this.image} />
+        <p class="paragraph">
+          This image depicts a strapping young Penn State student reacting to an
+          insane moment in his favorite class: IST 256. He can not believe what
+          he is seeing!
+        </p>
+        <button class="details" @click=${this.details}>Details</button>
+      </div>
+      <meme-maker alt="Cat stalking a small toy" image-url="https://cdn2.thecatapi.com/images/9j5.jpg" top-text="I bring you" bottom-text="the death">
+</meme-maker>
     `;
   }
+
+  copy(event) {
+    const clone = this.shadowRoot.querySelector('.card').cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+    this.cardCount++;
+  }
+
+  delete(event) {
+    if (this.cardCount > 1) {
+      this.shadowRoot.querySelector('.card:last-child').remove();
+      this.cardCount--;
+    }
+  }
+
+  changeTitle(event) {
+    if (
+      this.shadowRoot.querySelector('.header').innerHTML === 'Bryce Schneider'
+    ) {
+      this.shadowRoot.querySelector('.header').innerHTML = 'Something Else';
+    } else {
+      this.shadowRoot.querySelector('.header').innerHTML = 'Bryce Schneider';
+    }
+  }
+
+  changeBG(event) {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.shadowRoot.querySelector('.card').style.backgroundColor = color;
+  }
+
+  details(event) {
+    if (this.shadowRoot.querySelector('.paragraph').style.display === 'none') {
+      this.shadowRoot.querySelector('.paragraph').style.display = 'block';
+    } else {
+      this.shadowRoot.querySelector('.paragraph').style.display = 'none';
+    }
+  }
+
 }
 
 customElements.define('city-card', CityCard);
