8f1b37c6ff51c96ce025bf4ff5b140e11afe838a
NewJerkey <bryschneider1@gmail.com>
2023-02-19 15:00:38 -0500

    added index.html stuff

--- src/city-card.js
+++ src/city-card.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
 //const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
@@ -6,13 +6,16 @@ import "@lrnwebcomponents/meme-maker/meme-maker.js";
 class CityCard extends LitElement {
   static properties = {
     title: { type: String },
-    cardCount: { type: Int16Array },
-    image: { type: String }
-  }
+    image: { type: String },
+    topLabel: { type: String },
+  };
 
   static styles = css`
     body {
       background: grey;
+    }
+    .wrapper {
+      display: inline-flex;
     }
     .card {
       width: 300px;
@@ -62,7 +65,8 @@ class CityCard extends LitElement {
 
     .image {
       border: 3px solid yellow;
-      max-width: 250px;
+      max-width: 300px;
+      text-align: center;
     }
 
     @media (max-width: 800px) and (min-width: 500px) {
@@ -80,79 +84,33 @@ class CityCard extends LitElement {
 
   constructor() {
     super();
-    this.cardCount = 1;
-    this.title = 'Bryce Schneider';
+    this.title = "Bryce Schneider";
     this.image =
-      'https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png';
+      "https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png";
+    this.topLabel = "Handsome Bryce";
   }
 
   render() {
     return html`
-      <div class="buttons">
-        <button class="copy" @click=${this.copy}>Duplicate</button>
-        <button class="changeBackground" @click=${this.changeBG}>
-          Change Background
-        </button>
-        <button class="changeTitle" @click=${this.changeTitle}>
-          Change Title
-        </button>
-        <button id="delete" @click=${this.delete}>Delete Last Instance</button>
+      <div class="wrapper">
+        <div class="card">
+          <h2 class="header">${this.title}</h2>
+          <meme-maker
+            class="image"
+            alt=${this.topLabel}
+            image-url=${this.image}
+            top-text=${this.topLabel}
+          ></meme-maker>
+          <p class="paragraph">
+            This image depicts a strapping young Penn State student reacting to
+            an insane moment in his favorite class: IST 256. He can not believe
+            what he is seeing!
+          </p>
+          <button class="details" @click=${this.details}>Details</button>
+        </div>
       </div>
-      <div class="card">
-        <h2 class="header">${this.title}</h2>
-        <img class="image" alt="Handsome Bryce" src=${this.image} />
-        <p class="paragraph">
-          This image depicts a strapping young Penn State student reacting to an
-          insane moment in his favorite class: IST 256. He can not believe what
-          he is seeing!
-        </p>
-        <button class="details" @click=${this.details}>Details</button>
-      </div>
-      <meme-maker alt="Cat stalking a small toy" image-url="https://cdn2.thecatapi.com/images/9j5.jpg" top-text="I bring you" bottom-text="the death">
-</meme-maker>
     `;
   }
-
-  copy(event) {
-    const clone = this.shadowRoot.querySelector('.card').cloneNode(true);
-    this.shadowRoot.appendChild(clone);
-    this.cardCount++;
-  }
-
-  delete(event) {
-    if (this.cardCount > 1) {
-      this.shadowRoot.querySelector('.card:last-child').remove();
-      this.cardCount--;
-    }
-  }
-
-  changeTitle(event) {
-    if (
-      this.shadowRoot.querySelector('.header').innerHTML === 'Bryce Schneider'
-    ) {
-      this.shadowRoot.querySelector('.header').innerHTML = 'Something Else';
-    } else {
-      this.shadowRoot.querySelector('.header').innerHTML = 'Bryce Schneider';
-    }
-  }
-
-  changeBG(event) {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    this.shadowRoot.querySelector('.card').style.backgroundColor = color;
-  }
-
-  details(event) {
-    if (this.shadowRoot.querySelector('.paragraph').style.display === 'none') {
-      this.shadowRoot.querySelector('.paragraph').style.display = 'block';
-    } else {
-      this.shadowRoot.querySelector('.paragraph').style.display = 'none';
-    }
-  }
-
 }
 
-customElements.define('city-card', CityCard);
+customElements.define("city-card", CityCard);
